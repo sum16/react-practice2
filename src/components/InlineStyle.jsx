720ca8d0c93eed2c5a8806aec41ec9439e2204cd
yasuyuki sato <66477859+sum16@users.noreply.github.com>
2021-09-22 01:11:27 +0900

    Initial commit

---
+++ src/components/InlineStyle.jsx
@@ -0,0 +1,31 @@
+export const InlineStyle = () => {
+  const containerStyle = {
+    // プロパティ名:値は""で文字列にする
+    // プロパティはキャメルケースで記述
+    border: "solid 2px #392eff",
+    borderRadius: "20px",
+    padding: "8px",
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center"
+  };
+
+  const titleStyle = {
+    margin: "0",
+    color: "#3d84a8"
+  };
+
+  const buttonStyle = {
+    backgroundColor: "#abedd8",
+    border: "none",
+    padding: "8px",
+    borderRadius: "8px"
+  };
+
+  return (
+    <div style={containerStyle}>
+      <p style={titleStyle}>-Inline-style</p>
+      <button style={buttonStyle}>FIGHT!</button>
+    </div>
+  );
+};
